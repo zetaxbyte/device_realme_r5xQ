f51b073fe4148afe9fbadfd4085dcef5c2d7de92
zetaxbyte <142310052+zetaxbyte@users.noreply.github.com>
2024-06-27 10:49:58 +0800

    Update and rename nad_r5xQ.mk to ancient_r5xQ.mk

--- ancient_r5xQ.mk
+++ ancient_r5xQ.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Inherit from r5xQ device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -23,7 +23,7 @@ USE_PIXEL_CHARGING := true
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := r5xQ
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := nad_r5xQ
+PRODUCT_NAME := ancient_r5xQ
 PRODUCT_MODEL := Realme r5xQ Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
